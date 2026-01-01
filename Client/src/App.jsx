e25f240740eee6bc4093c922270c9446e17f2508
SalmanHossen <hossainsalman873@gmail.com>
2026-01-01 14:22:04 +0600

    navbar compoent done

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,13 @@
+import { useState } from 'react';
 import Navbar from './components/Navbar';
+import { useLocation } from 'react-router-dom';
 
 const App = () => {
+  const [showLogin,setShowLogin]=useState(false);
+  const isOwnerPath=useLocation().pathname.startsWith('/owner')
   return (
     <div>
-      <Navbar/>
+      {!isOwnerPath && <Navbar setShowLogin={setShowLogin}/>}
     </div>
   )
 }
