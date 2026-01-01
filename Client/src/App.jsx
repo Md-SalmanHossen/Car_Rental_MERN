7773f3bbd7394b5257ee96eba255f44ec875570e
SalmanHossen <hossainsalman873@gmail.com>
2026-01-01 14:55:03 +0600

    create routing setup

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,14 +1,26 @@
 import { useState } from 'react';
 import Navbar from './components/Navbar';
-import { useLocation } from 'react-router-dom';
+import { Route,Routes, useLocation } from 'react-router-dom';
+import Home from './pages/Home';
+import CarDetails from './pages/CarDetails';
+import Cars from './pages/Cars';
+import MyBooking from './pages/MyBooking';
 
 const App = () => {
   const [showLogin,setShowLogin]=useState(false);
   const isOwnerPath=useLocation().pathname.startsWith('/owner')
   return (
-    <div>
+    <>
       {!isOwnerPath && <Navbar setShowLogin={setShowLogin}/>}
-    </div>
+      
+      <Routes>
+        <Route path='' element={<Home/>}/>
+        <Route path='/car-details/:id' element={<CarDetails/>}/>
+        <Route path='/cars' element={<Cars/>}/>
+        <Route path='/my-bookings' element={<MyBooking/>}/>
+      </Routes>
+      
+    </>
   )
 }
 
