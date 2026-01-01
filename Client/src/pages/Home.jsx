c2b029741580a1f17f39b7558e72e12c15796f01
SalmanHossen <hossainsalman873@gmail.com>
2026-01-01 23:08:47 +0600

    create Banner section

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
 import Feature from '../components/Feature'
+import Banner from '../components/Banner'
 
 const Home = () => {
   return (
     <div>
       <HeroSection/>
       <Feature/>
+      <Banner/>
     </div>
   )
 }
