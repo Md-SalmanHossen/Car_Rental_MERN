fe6872fc2d96d7859d5e737fdd1c48fc19177744
SalmanHossen <hossainsalman873@gmail.com>
2026-01-01 22:34:52 +0600

    card section done

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
+import Feature from '../components/Feature'
 
 const Home = () => {
   return (
     <div>
       <HeroSection/>
+      <Feature/>
     </div>
   )
 }
