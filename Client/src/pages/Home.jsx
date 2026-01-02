7efd7fe15fecb1461473f739da154ac4aff94e3e
SalmanHossen <hossainsalman873@gmail.com>
2026-01-02 20:18:13 +0600

    Newsletter done

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import HeroSection from '../components/HeroSection'
 import Feature from '../components/Feature'
 import Banner from '../components/Banner'
+import Testimonial from '../components/Testimonial'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <HeroSection/>
       <Feature/>
       <Banner/>
+      <Testimonial/>
     </div>
   )
 }
