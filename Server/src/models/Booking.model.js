86e7cdb5f4aa459b4dcb57697cd06f19e2ccc777
SalmanHossen <hossainsalman873@gmail.com>
2026-01-06 11:23:30 +0600

    create booking controller

---
+++ Server/src/models/Booking.model.js
@@ -0,0 +1,14 @@
+import mongoose, { Types } from "mongoose";
+
+const bookingSchema=new mongoose.Schema({
+   car:{type:mongoose.Schema.Types.ObjectId,ref:'Car',required:true},
+   user:{type:mongoose.Schema.Types.ObjectId,ref:'User',required:true},
+   owner:{type:mongoose.Schema.Types.ObjectId,ref:'User',required:true},
+   pickup_date:{type:Date,required:true},
+   return_date:{type:Date,required:true},
+   return_date:{type:String,enum:['pending',confirmed,cancelled],default:'pending'},
+   price:{type:Number,required:true}
+},{timestamps:true});
+
+const Booking=mongoose.model('Bookings',bookingSchema);
+export default Booking;
